3468e898d727e15ea523babcaa89e9c2d61a644a
apolobed <ikodane@yandex.ru>
2025-04-22 11:38:00 +0400

    first commit

---
+++ phase portrait/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace third
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.glControl1 = new OpenTK.GLControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.trackBar4 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.trackBar5 = new System.Windows.Forms.TrackBar();
+            this.trackBar6 = new System.Windows.Forms.TrackBar();
+            this.trackBar7 = new System.Windows.Forms.TrackBar();
+            this.trackBar8 = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar8)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // glControl1
+            // 
+            this.glControl1.BackColor = System.Drawing.Color.Black;
+            this.glControl1.Location = new System.Drawing.Point(13, 38);
+            this.glControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.glControl1.Name = "glControl1";
+            this.glControl1.Size = new System.Drawing.Size(800, 400);
+            this.glControl1.TabIndex = 0;
+            this.glControl1.VSync = false;
+            this.glControl1.Load += new System.EventHandler(this.glControl1_Load);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(40, 576);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 1;
+            this.trackBar1.Location = new System.Drawing.Point(40, 626);
+            this.trackBar1.Maximum = 12;
+            this.trackBar1.Minimum = -12;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(117, 56);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.LargeChange = 1;
+            this.trackBar2.Location = new System.Drawing.Point(40, 669);
+            this.trackBar2.Maximum = 12;
+            this.trackBar2.Minimum = -12;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(117, 56);
+            this.trackBar2.TabIndex = 3;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.LargeChange = 1;
+            this.trackBar3.Location = new System.Drawing.Point(868, 63);
+            this.trackBar3.Maximum = 400;
+            this.trackBar3.Minimum = 20;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(117, 56);
+            this.trackBar3.TabIndex = 4;
+            this.trackBar3.Value = 20;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // trackBar4
+            // 
+            this.trackBar4.LargeChange = 1;
+            this.trackBar4.Location = new System.Drawing.Point(868, 137);
+            this.trackBar4.Maximum = 20;
+            this.trackBar4.Minimum = 1;
+            this.trackBar4.Name = "trackBar4";
+            this.trackBar4.Size = new System.Drawing.Size(117, 56);
+            this.trackBar4.TabIndex = 5;
+            this.trackBar4.Value = 1;
+            this.trackBar4.Scroll += new System.EventHandler(this.trackBar4_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(905, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "N";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(905, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "T";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // trackBar5
+            // 
+            this.trackBar5.Location = new System.Drawing.Point(868, 211);
+            this.trackBar5.Maximum = 200;
+            this.trackBar5.Minimum = -200;
+            this.trackBar5.Name = "trackBar5";
+            this.trackBar5.Size = new System.Drawing.Size(117, 56);
+            this.trackBar5.TabIndex = 8;
+            this.trackBar5.Scroll += new System.EventHandler(this.trackBar5_Scroll);
+            // 
+            // trackBar6
+            // 
+            this.trackBar6.Location = new System.Drawing.Point(868, 283);
+            this.trackBar6.Maximum = 200;
+            this.trackBar6.Minimum = -200;
+            this.trackBar6.Name = "trackBar6";
+            this.trackBar6.Size = new System.Drawing.Size(117, 56);
+            this.trackBar6.TabIndex = 9;
+            this.trackBar6.Scroll += new System.EventHandler(this.trackBar6_Scroll);
+            // 
+            // trackBar7
+            // 
+            this.trackBar7.Location = new System.Drawing.Point(868, 360);
+            this.trackBar7.Maximum = 200;
+            this.trackBar7.Minimum = -200;
+            this.trackBar7.Name = "trackBar7";
+            this.trackBar7.Size = new System.Drawing.Size(117, 56);
+            this.trackBar7.TabIndex = 10;
+            this.trackBar7.Scroll += new System.EventHandler(this.trackBar7_Scroll);
+            // 
+            // trackBar8
+            // 
+            this.trackBar8.Location = new System.Drawing.Point(868, 433);
+            this.trackBar8.Maximum = 200;
+            this.trackBar8.Minimum = -200;
+            this.trackBar8.Name = "trackBar8";
+            this.trackBar8.Size = new System.Drawing.Size(117, 56);
+            this.trackBar8.TabIndex = 11;
+            this.trackBar8.Scroll += new System.EventHandler(this.trackBar8_Scroll);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(905, 196);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 16);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "a_11";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(905, 264);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 16);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "a_12";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(905, 341);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 16);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "a_21";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(905, 414);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 16);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "a_22";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1022, 753);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.trackBar8);
+            this.Controls.Add(this.trackBar7);
+            this.Controls.Add(this.trackBar6);
+            this.Controls.Add(this.trackBar5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar4);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.glControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar8)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private OpenTK.GLControl glControl1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.TrackBar trackBar4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar trackBar5;
+        private System.Windows.Forms.TrackBar trackBar6;
+        private System.Windows.Forms.TrackBar trackBar7;
+        private System.Windows.Forms.TrackBar trackBar8;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ phase portrait/Form1.cs
@@ -0,0 +1,452 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenTK.Graphics.OpenGL;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace third
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        double w = 10;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GL.ClearColor(0.0f, 0.0f, 0.0f, 1.0f); //цвет очистки экрана
+            GL.MatrixMode(MatrixMode.Projection);   //матрица проекции
+            GL.LoadIdentity();                      // вместо текущей матрицы загружается единичная матриц
+            GL.Ortho(-w, 3*w, -w, w, -1, 1);
+            GL.MatrixMode(MatrixMode.Modelview);    //модельновидовая матрица (текущая). Отвечает за геом.преобразования. Сначала все точки (vector) умножаются на модельновидовую матрицу слева (на единичную), затем умножается на матрицу проекции слева. Т.о. происходит отображение точек так как они есть.
+                                                    //Существуют функции которые изменяют проекцию вектора. GL.Rotate() - поворот на угол вокруг вектора. GL.Translate() - параллельный перенос на вектор (ко всем координатам будет прибавлен вектор). GL.Scale() - масштабирование. Все эти преобразования являются матрицами преобразований, на которые умножается модельновидовая матрица справа.
+                                                    //Преобразования выполняются в обратном порядке вызова, т.к. домножаюся на текущую матрицу справа. Преобразования накапливаются.
+                                                    //Все эти матрицы 4х4. Можно самим создавать (определять матрицы)
+                                                    //Если нужно сохранить результат, чтобы далее по-разному его преобразовывать, есть команды GL.PushMatrix() - сохранить текущую матрицу, GL.PopMatrix() - удалить текущую матрицу.
+            GL.LoadIdentity();
+        }
+
+        void Coord()
+        {
+            double h1 = w / 2; // малые деления
+            double h2 = h1 / 5; // средние деления
+
+            GL.LineWidth(1.0f);
+
+            GL.Begin(PrimitiveType.Lines);
+
+            // малые разделения
+            GL.Color3(0.2f, 0.2f, 0.2f);
+
+            for (double x = 0; x <= w; x += h2)
+            {
+                // по 4 линии за шаг
+                GL.Vertex2(x, -w);
+                GL.Vertex2(x, w);
+                GL.Vertex2(-x, -w);
+                GL.Vertex2(-x, w);
+                GL.Vertex2(-w, x);
+                GL.Vertex2(w, x);
+                GL.Vertex2(-w, -x);
+                GL.Vertex2(w, -x);
+            }
+
+            // средние разделения
+            GL.Color3(0.45f, 0.45f, 0.45f);
+
+            for (double x = 0; x <= w; x += h1)
+            {
+                // по 4 линии за шаг
+                GL.Vertex2(x, -w);
+                GL.Vertex2(x, w);
+                GL.Vertex2(-x, -w);
+                GL.Vertex2(-x, w);
+                GL.Vertex2(-w, x);
+                GL.Vertex2(w, x);
+                GL.Vertex2(-w, -x);
+                GL.Vertex2(w, -x);
+            }
+
+            // оси
+            GL.Color3(1.0f, 1.0f, 1.0f);
+
+            GL.Vertex2(-w, 0);
+            GL.Vertex2(w, 0);
+            GL.Vertex2(0, -w);
+            GL.Vertex2(0, w);
+
+            GL.End();
+
+            // стрелочки (вытянуты и их длина зависит от области видимости)
+            GL.Begin(PrimitiveType.Triangles);
+            // по оси х
+            GL.Vertex2(w, 0);
+            GL.Vertex2(w - w / 20, w / 40);
+            GL.Vertex2(w - w / 20, -w / 40);
+            // по оси у
+            GL.Vertex2(0, w);
+            GL.Vertex2(w / 40, w - w / 20);
+            GL.Vertex2(-w / 40, w - w / 20);
+
+            GL.End();
+        }
+
+        
+
+
+        int N = 400;
+
+        double x0 = 1;
+        double y0 = 1;
+
+
+        double alpha = 0;
+        double beta = 40;
+        double T;
+        double C1(double x0, double y0)
+        {
+            return 4 * y0 - 2 * x0;
+        }
+
+        double C2(double x0, double y0)
+        {
+            return 2 * x0 - 3 * y0;
+        }
+
+        double x_common(double t)
+        {
+            return C1(x0, y0) * Math.Exp(-t) * 3 / 2 + C2(x0, y0) * Math.Exp(-t / 2) * 2;
+        }
+
+        double y_common(double t)
+        {
+            return C1(x0, y0) * Math.Exp(-t) + C2(x0, y0) * Math.Exp(-t / 2);
+        }
+        double a_11, a_12, a_21, a_22;
+        double func_1(double x, double y)
+        {
+            ///return x - 3 * y;
+            return a_11 * x*x -  a_12 * y;
+        }
+
+        double func_2(double x, double y)
+        {
+            ///return x - 5 * y / 2;
+            return a_21 * x - a_22 * y*y;
+
+        }
+
+        /// y1 = x; y2 = y; y3 = x_; y4 = y_;
+
+        /// y1_ = y3; y2_ = y4; y3_ =  a_11 * x -  a_12 * y; y4_ = a_21 * x - a_22 * ;
+
+
+
+        double g1(double y1, double y2, double y3, double y4)
+        {
+            return y3;
+        }
+        double g2(double y1, double y2, double y3, double y4)
+        {
+            return y4;
+        }
+
+        
+         double g3(double y1, double y2, double y3, double y4)
+        {
+            return a_11 * y1*y1 - a_12 * y2;
+        }
+        double g4(double y1, double y2, double y3, double y4)
+        {
+            return a_21 * y1 - a_22 * y2*y2;
+        }
+
+        double y1_0 = 1;
+        double y2_0 = 1;
+        double y3_0 = 0;
+        double y4_0 = 0;
+
+        
+        double[,] GetAproximateSolution1(double y1_0, double y2_0, double y3_0, double y4_0)
+        {
+            ///double T = beta - alpha;
+            double[,] AprS = new double[N + 1, 4];
+            double dt = T / N;
+            AprS[0, 0] = y1_0;
+            AprS[0, 1] = y2_0;
+            AprS[0, 2] = y3_0;
+            AprS[0, 3] = y4_0;
+
+            for (int i = 0; i<N; i++)
+            {
+                AprS[i + 1, 0] = AprS[i, 0] + dt * g1(AprS[i, 0], AprS[i, 1], AprS[i, 2], AprS[i, 3]);
+                AprS[i + 1, 1] = AprS[i, 1] + dt * g2(AprS[i, 0], AprS[i, 1], AprS[i, 2], AprS[i, 3]);
+                AprS[i + 1, 2] = AprS[i, 2] + dt * g3(AprS[i, 0], AprS[i, 1], AprS[i, 2], AprS[i, 3]);
+                AprS[i + 1, 3] = AprS[i, 3] + dt * g4(AprS[i, 0], AprS[i, 1], AprS[i, 2], AprS[i, 3]);
+            }
+
+            return AprS;
+        }
+        List<double[,]> Fase_Portrait1;
+        List<double[,]> Fase_Portrait0;
+        double x_0 = 1;
+        double y_0 = 0;
+        double[,] GetAproximateSolution0(double x_0, double y_0)
+        {
+            ///double T = beta - alpha;
+            double[,] AprS = new double[N + 1, 2];
+            double dt = T / N;
+            AprS[0, 0] = x_0;
+            AprS[0, 1] = y_0;
+            
+
+            for (int i = 0; i < N; i++)
+            {
+                AprS[i + 1, 0] = AprS[i, 0] + dt * func_1(AprS[i, 0], AprS[i, 1]);
+                AprS[i + 1, 1] = AprS[i, 1] + dt * func_2(AprS[i, 0], AprS[i, 1]);
+            }
+
+            return AprS;
+        }
+
+        void Calc_Fase_Portrait1()
+        {
+            Fase_Portrait1 = new List<double[,]>();
+
+            for (int y1 = -10; y1 <= 10; y1++)
+            {
+                for (int y2 = -10; y2 <= 10; y2++)
+                {
+                    
+                    Fase_Portrait1.Add(GetAproximateSolution1(y1, y2, y3_0, y4_0));
+                }
+            }
+        }
+        void Calc_Fase_Portrait0()
+        {
+            Fase_Portrait0 = new List<double[,]>();
+
+            for(int x = -10; x<=10; x++)
+            {
+                for(int y=-10; y<=10; y++)
+                {
+                    Fase_Portrait0.Add(GetAproximateSolution0(x, y));
+                }
+            }
+        }
+
+        void Fase_Portrait_Graphic1()
+        {
+
+            for(int i = 0; i<Fase_Portrait1.Count; i++)
+            {
+                GL.Begin(PrimitiveType.LineStrip);
+
+                for (int j = 0; j < N; j++)
+                {
+                    if (Fase_Portrait1[i][j, 2] >= -w)
+                    {
+                        GL.Vertex2(Fase_Portrait1[i][j, 2], Fase_Portrait1[i][j, 3]);
+                    }
+                                        
+                }
+
+                GL.End();
+            }
+        }
+        void Fase_Portrait_Graphic0()
+        {
+
+            for (int i = 0; i < Fase_Portrait0.Count; i++)
+            {
+                GL.Begin(PrimitiveType.LineStrip);
+
+                for (int j = 0; j < N; j++)
+
+                {
+                    if(Fase_Portrait0[i][j, 0] < w)
+                    {
+                        GL.Vertex2(Fase_Portrait0[i][j, 0], Fase_Portrait0[i][j, 1]);
+                    }
+                    
+
+
+                }
+
+                GL.End();
+            }
+        }
+        /*void AprSGraphic()
+        {
+            double[,] x = GetAproximateSolution(x0, y0);
+
+            GL.Color3(1f, 0.0f, 0.5f);
+            GL.LineWidth(2);
+
+            GL.Begin(PrimitiveType.LineStrip);
+
+            for (int i = 0; i < N; i++)
+            {
+                GL.Vertex2(x[i, 0], x[i, 1]);
+            }
+
+            GL.End();
+        }*/
+        int N1 = 200;
+        void RealSGraphic()
+        {
+            ///double T = beta - alpha;
+            double dt = T / N1;
+
+            GL.Color3(1.0f, 1.0f, 1.0f);
+            GL.LineWidth(2);
+
+            GL.Begin(PrimitiveType.LineStrip);
+
+            for (double t = 0; t < T; t += dt)
+            {
+                GL.Vertex2(x_common(t), y_common(t));
+            }
+
+            GL.End();
+        }
+
+        double[,] AprS;
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ///Draw();
+        }
+
+        private void glControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        void Draw()
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit); // очистка буфера цвета
+
+            Coord();
+
+            GL.PushMatrix();
+
+            GL.Scale(2, 2, 1);
+
+            ///AprSGraphic();
+            ///RealSGraphic();
+            GL.PopMatrix();
+            Fase_Portrait_Graphic0();
+
+            GL.PushMatrix();
+            GL.Translate(2 * w, 0, 0);
+            Coord();
+            Fase_Portrait_Graphic1();
+            GL.PopMatrix();
+
+            glControl1.SwapBuffers(); //перемена буфера местами
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            x0 = trackBar1.Value;
+            y0 = trackBar2.Value;
+            N = trackBar3.Value;
+            T = trackBar4.Value;
+            a_11 = trackBar5.Value / 100.0;
+            label3.Text = "a_11: " + a_11.ToString();
+            a_12 = trackBar6.Value / 100.0;
+            label4.Text = "a_12: " + a_12.ToString();
+            a_21 = trackBar7.Value / 100.0;
+            label5.Text = "a_21: " + a_21.ToString();
+            a_22 = trackBar8.Value / 100.0;
+            label6.Text = "a_22: " + a_22.ToString();
+
+            Calc_Fase_Portrait1();
+            Calc_Fase_Portrait0();
+
+            Draw();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar4_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar7_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar5_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar6_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar8_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
